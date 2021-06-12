591e2ab1557800f2337a652bfe033318fa535dbd
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-06-12 14:01:03 -0300

    Versão incial de resultados de cards

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -8,11 +8,39 @@
     />
     {{ climateZone }}
   </label>
+
+  <div v-for="filter in usedFilters" :key="filter">
+    {{ filter }}
+    <label v-for="optFilter in filters[filter]" :key="optFilter">
+      <input type="checkbox" name="selected" :value="optFilter" />
+      {{ optFilter }}
+    </label>
+  </div>
+
+  <div v-for="card in cards" :id="`card-${card.id}`" :key="card.id">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Payback</th>
+          <th>Incremental Cost</th>
+        </tr>
+      </thead>
+      <tr v-for="measure in card.measures" :key="measure.name">
+        <td>{{ measure.name }}</td>
+        <td>{{ measure.simplePayback }}</td>
+        <td>{{ measure.incrementalCost }}</td>
+      </tr>
+    </table>
+
+    <hr />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, reactive, ref } from "vue";
 import { useStore } from "vuex";
+import { findCards } from "../services/cards";
 
 const Results = defineComponent({
   name: "Results",
@@ -25,7 +53,22 @@ const Results = defineComponent({
     const selectedClimateZone =
       store.state.filters.climateZone || climateZones[0];
 
-    return { climateZones, selectedClimateZone };
+    const filters = reactive({
+      buildings: ["Building1", "Building2"],
+      types: ["Type1", "Type2"],
+    });
+
+    const cards = ref(findCards());
+
+    const usedFilters = computed(() => Object.keys(filters));
+
+    return {
+      climateZones,
+      selectedClimateZone,
+      usedFilters,
+      filters,
+      cards,
+    };
   },
 });
 
